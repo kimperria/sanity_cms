9852a1c4260ddc39530e5cdad40eab782d20817b
kimperria <kimwaweru100@gmail.com>
2025-04-06 15:59:48 +0300

    feat: add custom validation and update editorial experience

--- schemaTypes/propertyType.ts
+++ schemaTypes/propertyType.ts
@@ -1,13 +1,27 @@
 import {defineField, defineType} from 'sanity'
+import {HomeIcon} from '@sanity/icons'
 
 export const propertyType = defineType({
   name: 'property',
   title: 'Property',
   type: 'document',
+  icon: HomeIcon,
   fields: [
     defineField({
       name: 'name',
       type: 'string',
+      validation: (rule) => rule.required().error('Required to list item on a page on the webite'),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'name'},
+      validation: (rule) => rule.required().error('Required to generate a page on the webite'),
+      hidden: ({document}) => !document?.name,
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
     }),
     defineField({
       name: 'size_in_hectares',
@@ -20,16 +34,21 @@ export const propertyType = defineType({
     }),
     defineField({
       name: 'price',
+      description: 'Estimate cost in kenyan shillings',
       type: 'number',
     }),
     defineField({
-      name: 'image',
-      type: 'image',
-    }),
-    defineField({
       name: 'owner',
+      description: 'This could a caretaker, landlord or any other contact person',
       type: 'reference',
       to: [{type: 'owner'}],
     }),
   ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'owner.client_name',
+      media: 'image',
+    },
+  },
 })
